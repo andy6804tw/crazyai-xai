2525e234add44c1a86856a87e5bf5ea3993d995d
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2024-09-04 22:32:15 +0800

    Deployed 1cac0b1 with MkDocs version: 1.0.4

---
+++ javascripts/analytics.js
@@ -0,0 +1,10 @@
+var script = document.createElement('script');
+script.src = 'https://www.googletagmanager.com/gtag/js?id=G-3T4T4Q0T4K';
+script.async = true;
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-3T4T4Q0T4K');
